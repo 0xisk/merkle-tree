ae0ff51cbd1696f212c8ded36d6c907b8bcd1c15
0xisk <0xisk@proton.me>
2024-01-12 12:35:38 +0100

    fix: remove console.logs

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ use ark_serialize::CanonicalSerialize;
 use num_bigint::BigUint;
 use poseidon::constants::secp256k1_w3;
 use wasm_bindgen::prelude::wasm_bindgen;
-use web_sys::console;
 
 use crate::merkle_tree::{MerkleProofBytes, MerkleTree};
 
@@ -17,12 +16,8 @@ fn internal_generate_merkle_proof<F: PrimeField>(
     leaf: String,
     depth: usize,
 ) -> MerkleProofBytes {
-    console::log_1(&"Starting internal_generate_merkle_proof".into());
     let leaves = leaves.clone();
 
-    print!("Merkle_Tree_Create: leaves len {:?}", leaves.len());
-
-    console::log_1(&"Starting padded_leaves".into());
     let mut padded_leaves = leaves.clone();
     // Pad the leaves to equal the size of the tree
     padded_leaves.resize(1 << depth, "0".to_string());
@@ -30,32 +25,23 @@ fn internal_generate_merkle_proof<F: PrimeField>(
     const ARITY: usize = 2;
     const WIDTH: usize = ARITY + 1;
 
-    console::log_1(&"Starting tree".into());
     let mut tree = MerkleTree::<F, WIDTH>::new(secp256k1_w3());
 
-    console::log_1(&"Starting padded_leaves".into());
     // Insert all the leaves into the tree
     for leaf in &padded_leaves {
         // Converting String to F
         let leaf_hex = hex::decode(leaf.replace("0x", "to")).unwrap();
-        console::log_1(&"Starting leaf_bytes".into());
         let leaf_bytes = F::from(BigUint::from_bytes_be(&leaf_hex));
-        console::log_1(&"Starting insert".into());
         tree.insert(leaf_bytes);
     }
 
-    console::log_1(&"Starting tree finish".into());
     tree.finish();
-
-    console::log_1(&"Starting tree finish".into());
 
     let leaf_hex = hex::decode(leaf.replace("0x", "")).unwrap();
     let leaf_prime_field = F::from(BigUint::from_bytes_be(&leaf_hex));
 
-    console::log_1(&"Starting creating proof".into());
     let proof = tree.create_proof(leaf_prime_field);
 
-    console::log_1(&"Starting converting to bytes".into());
     // Encode the Merkle Proof output with BigEndian
     let mut merkle_siblings = Vec::with_capacity(1 * depth);
     let mut merkle_indices = Vec::with_capacity(1 * depth);
@@ -92,18 +78,13 @@ pub fn generate_merkle_proof(leaves: Vec<String>, leaf: String, depth: usize) ->
 
     type F = ark_secp256k1::Fq;
 
-    console::log_1(&"Starting generate_merkle_proof".into());
-
     let merkle_proof_bytes = internal_generate_merkle_proof::<F>(leaves, leaf, depth);
 
-    console::log_1(&"Starting serializing".into());
     // Serialize the full merkle proof
     let mut merkle_proof_bytes_serialized = Vec::new();
     merkle_proof_bytes
         .serialize_compressed(&mut merkle_proof_bytes_serialized)
         .unwrap();
-
-    console::log_1(&"Starting creating proof".into());
 
     merkle_proof_bytes_serialized
 }
